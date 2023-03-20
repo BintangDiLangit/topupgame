10479a020479b58764db00e1f603689b29446973
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2023-03-21 01:10:40 +0700

    add verifycsrftoken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/in-callback',
-        '/out-callback'
+        '/out-callback',
+        '/apigames/transaksi/callback'
     ];
 }
