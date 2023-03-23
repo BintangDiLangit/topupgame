cc0aa9444fa9730b40cadeba1dc1a2160e0cf4ee
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2023-03-23 14:00:33 +0700

    update 5

--- database/migrations/2023_03_22_233318_add_harga_jual_to_produks_table.php
+++ database/migrations/2023_03_22_233318_add_harga_jual_to_produks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('produks', function (Blueprint $table) {
-            $table->string('harga_jual');
+            $table->string('harga_jual')->nullable();
         });
     }
 
