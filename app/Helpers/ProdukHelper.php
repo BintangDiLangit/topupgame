22de374d17790b0aaa83891221845e0f24c9a717
BintangDiLangit <supercellbanned1@gmail.com>
2023-04-03 15:05:01 +0700

    change untung

--- app/Helpers/ProdukHelper.php
+++ app/Helpers/ProdukHelper.php
@@ -12,7 +12,7 @@ class ProdukHelper
     public function __construct()
     {
         $this->kursSoc = 294;
-        $this->untung = 0.075;
+        $this->untung = 0.1;
 
     }
     public function listPAdmin()
